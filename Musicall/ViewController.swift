bffdfedca011e98a729240d7be38a8c4e79e1b5c
vBryann <vitorbryan_melo@hotmail.com>
2021-09-09 10:55:24 -0300

    Initial Commit

---
+++ Musicall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musicall
+//
+//  Created by Vitor Bryan on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
