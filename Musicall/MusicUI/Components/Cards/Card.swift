2574e87f1b408670f20c94973bf399e79abf0ff6
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-14 10:40:26 -0300

    Class Struct

---
+++ Musicall/MusicUI/Components/Cards/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Musicall
+//
+//  Created by Lucas Oliveira on 14/09/21.
+//
+
+import UIKit
+
+class Card: UIView {
+    // MARK: PUBLIC PROPERTIES
+    var isHighlighted = false
+    // MARK: PRIVATE PROPERTIES
+    // MARK: PUBLIC FUNCS
+    // MARK: PRIVATE FUNCS
+}
