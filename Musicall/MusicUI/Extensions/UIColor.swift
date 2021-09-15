226ba1a63a2a8e0756a93196bd5a263489335c65
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-15 09:44:37 -0300

    Add fonts and olors

---
+++ Musicall/MusicUI/Extensions/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  Musicall
+//
+//  Created by Lucas Fernandes on 14/09/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blue = #colorLiteral(red: 0.3176470588, green: 0.3803921569, blue: 0.9803921569, alpha: 1)
+    static let black = #colorLiteral(red: 0.1137254902, green: 0.1137254902, blue: 0.1490196078, alpha: 1)
+    static let darkestGray = #colorLiteral(red: 0.1764705882, green: 0.1803921569, blue: 0.2509803922, alpha: 1)
+    static let darkGray = #colorLiteral(red: 0.2196078431, green: 0.2470588235, blue: 0.3176470588, alpha: 1)
+    static let gray = #colorLiteral(red: 0.7529411765, green: 0.7529411765, blue: 0.7764705882, alpha: 1)
+    static let lightGray = #colorLiteral(red: 0.9137254902, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
+    static let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let red = #colorLiteral(red: 1, green: 0.3411764706, blue: 0.3529411765, alpha: 1)
+    static let cyan = #colorLiteral(red: 0.01568627451, green: 1, blue: 0.8941176471, alpha: 1)
+}
