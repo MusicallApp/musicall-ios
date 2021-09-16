0856cbba82e4f1104a8e0e262478050d677994c1
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-16 10:20:44 -0300

    add Heart

--- Musicall/MusicUI/Extensions/UIImage.swift
+++ Musicall/MusicUI/Extensions/UIImage.swift
@@ -12,6 +12,7 @@ extension UIImage {
     static let icContact = UIImage(named: "Contact")
     static let icHeadphone = UIImage(named: "Headphone")
     static let icHeartFill = UIImage(named: "Heart_Fill")
+    static let icHeart = UIImage(named: "Heart")
     static let icPaperclip = UIImage(named: "Paperclip")
     static let icPlus = UIImage(named: "Plus")
     static let icTelegram = UIImage(named: "Telegram")
