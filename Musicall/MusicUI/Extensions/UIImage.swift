86507b067b63ae1d5deaf3a2588b825f2ac91088
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-16 10:16:24 -0300

    add Icons

---
+++ Musicall/MusicUI/Extensions/UIImage.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage.swift
+//  Musicall
+//
+//  Created by Lucas Oliveira on 16/09/21.
+//
+
+import UIKit
+
+extension UIImage {
+    static let icComment = UIImage(named: "Comment")
+    static let icContact = UIImage(named: "Contact")
+    static let icHeadphone = UIImage(named: "Headphone")
+    static let icHeartFill = UIImage(named: "Heart_Fill")
+    static let icPaperclip = UIImage(named: "Paperclip")
+    static let icPlus = UIImage(named: "Plus")
+    static let icTelegram = UIImage(named: "Telegram")
+}
