56d2bc941e43796751718dfc2391f8307b41bc0c
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-09-17 10:57:43 -0300

    Add Float Action Sheet

---
+++ Musicall/MusicUI/Components/Elements/FloatActionSheet.swift
@@ -0,0 +1,75 @@
+//
+//  FloatActionSheet.swift
+//  Musicall
+//
+//  Created by Elias Ferreira on 15/09/21.
+//
+
+import UIKit
+import SnapKit
+
+class FloatActionSheet: UIView {
+
+    private let icon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .blue
+        
+        return imageView
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.MCDesignSystem(font: .body)
+        label.textColor = .white
+
+        return label
+    }()
+
+    init(imageIcon: UIImage?, title: String) {
+        super.init(frame: .zero)
+        backgroundColor = .darkestGray
+        icon.image = imageIcon
+        titleLabel.text = title
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        superview?.layoutSubviews()
+        layer.cornerRadius = 6
+    }
+
+    func addTarget(target: AnyObject, action: Selector?) {
+        let gesture = UITapGestureRecognizer(target: target, action: action)
+        addGestureRecognizer(gesture)
+    }
+
+    private func setupView() {
+        addSubview(icon)
+        addSubview(titleLabel)
+
+        /// Main view default size.
+        /// Can be changed on superview.
+        snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(50)
+            make.width.greaterThanOrEqualTo(200)
+        }
+
+        icon.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(14)
+            make.size.equalTo(20)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(icon.snp.trailing).offset(10)
+            make.trailing.equalToSuperview().inset(14)
+        }
+    }
+
+}
--- Musicall/ViewController.swift
+++ Musicall/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let floatActionSheet = FloatActionSheet(imageIcon: UIImage(systemName: "person.crop.circle"),
+                                            title: "Compartilhar meu perfil")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(floatActionSheet)
+
+        floatActionSheet.addTarget(target: self, action: #selector(test))
+
+        floatActionSheet.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
+    @objc
+    func test() {
+        print("Testando...")
+    }
 
 }
-
