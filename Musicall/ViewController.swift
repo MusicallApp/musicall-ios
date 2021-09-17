76556f46907d9bf25e7810b0240566c94cdbb1e7
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-17 11:59:03 -0300

    Update ViewController.swift

--- Musicall/ViewController.swift
+++ Musicall/ViewController.swift
@@ -9,31 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private let stackVertical: UIStackView = {
-        let stackView = UIStackView()
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.axis = .vertical
-        stackView.spacing = 0
-
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(stackVertical)
-        stackVertical.snp.makeConstraints { make in
-            make.left.right.equalToSuperview().inset(16)
-            make.centerY.equalToSuperview()
-        }
-
-        let card = SelectableCard(style: .musician)
-        card.titleText = "Contratante"
-        card.descriptionText = "Busca encontrar locais para tocar e quer fazer networking."
-        stackVertical.addArrangedSubview(card)
         // Do any additional setup after loading the view.
     }
+
 
 }
