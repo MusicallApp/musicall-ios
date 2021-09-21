f76d0c80451dc4269cf6471020f9c537dcec40ce
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-09-21 15:45:04 -0300

    Update ViewController.swift

--- Musicall/ViewController.swift
+++ Musicall/ViewController.swift
@@ -9,12 +9,8 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-        // Do any additional setup after loading the view.
-
     }
 }
