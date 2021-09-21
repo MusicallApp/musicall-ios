942063121ffab283cd96fff0a009bc3e15ed8ade
Vitor Bryan <vitorbryan_melo@hotmail.com>
2021-09-21 10:55:09 -0300

    FT-002 TableView created and CardTableViewCell

---
+++ Musicall/MusicApp/Mural/ViewController/CardTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  CardTableViewCell.swift
+//  Musicall
+//
+//  Created by Vitor Bryan on 21/09/21.
+//
+
+import UIKit
+
+class CardTableViewCell: UITableViewCell {
+    
+    static let identifier = "cardTableViewCell"
+    
+    fileprivate let cardView = Card(headerInfos: HeaderInfos(username: "Braia", date: "21/07"),
+                                    style: .simple(content: "asjkfgadhjsfgakj"))
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        addSubview(cardView)
+        setUpConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraints() {
+        cardView.snp.makeConstraints { (make) in
+            make.top.bottom.equalToSuperview().inset(8)
+            make.left.right.equalToSuperview()
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ Musicall/MusicApp/Mural/ViewController/MuralViewController.swift
@@ -0,0 +1,119 @@
+//
+//  MuralViewController.swift
+//  Musicall
+//
+//  Created by Vitor Bryan on 09/09/21.
+//
+
+import UIKit
+
+class MuralViewController: UIViewController {
+    
+    // MARK: Setup
+    
+//    let viewModel
+    
+    // MARK: UI
+    
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.backgroundColor = .clear
+        view.bounces = true
+        view.separatorStyle = .none
+        
+        return view
+    }()
+    
+    private let addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.layer.cornerRadius = 6
+        button.clipsToBounds = true
+        button.setImage(UIImage.icPlus, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        button.addTarget(self, action: #selector(addPost), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let topView = UIView()
+    
+    // MARK: Variables and constants
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpUI()
+        setUpTableView()
+    }
+    
+    // MARK: Methods
+    
+    @objc func addPost() {
+        
+    }
+}
+
+// MARK: UI Settings
+
+extension MuralViewController {
+    
+    private func setUpUI() {
+        // Navigation Controller
+        title = "Mural"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [
+                    NSAttributedString.Key.font: UIFont.MCDesignSystem(font: .heading1),
+                    NSAttributedString.Key.foregroundColor: UIColor.white
+                ]
+        let buttonItem = UIBarButtonItem(customView: addButton)
+        navigationItem.rightBarButtonItem = buttonItem
+        
+        view.backgroundColor = .black
+        
+        // Views
+        view.addSubview(tableView)
+        view.addSubview(topView)
+        
+        setUpConstraints()
+    }
+    
+    private func setUpConstraints() {
+        topView.snp.makeConstraints { (make) in
+            make.topMargin.equalToSuperview()
+            make.left.right.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.20)
+        }
+        
+        tableView.snp.makeConstraints { (make) in
+            make.topMargin.equalTo(topView.snp.bottom)
+            make.bottomMargin.equalToSuperview()
+            make.left.right.equalToSuperview().inset(16)
+        }
+    }
+    
+    public func setUpTableView() {
+        tableView.register(CardTableViewCell.self, forCellReuseIdentifier: CardTableViewCell.identifier)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MuralViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CardTableViewCell.identifier, for: indexPath)
+        guard cell is CardTableViewCell else { return cell }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UIScreen.main.bounds.height * 0.2
+    }
+    
+}
