89f1c9782b790448ff7d5665a2b4355a141d8d2c
Elias Ferreira <eliasferreira.pro@gmail.com>
2021-10-20 20:17:50 -0300

    Add confirm report view

---
+++ Musicall/MusicApp/ConfirmReport/ViewController/ConfirmReportViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ConfirmReportViewController.swift
+//  Musicall
+//
+//  Created by Elias Ferreira on 20/10/21.
+//
+
+import UIKit
+import SnapKit
+
+class ConfirmReportViewController: UIViewController, Coordinating {
+
+    var coordinator: Coordinator?
+
+    lazy var icon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "confirm")
+        return imageView
+    }()
+
+    lazy var subtitle: UILabel = {
+        let label = UILabel()
+        label.text = "Sua denúncia foi enviada com sucesso!"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .lightGray
+        label.font = .MCDesignSystem(font: .subtitle)
+        return label
+    }()
+
+    lazy var bodyText: UILabel = {
+        let label = UILabel()
+        label.text = "Obrigado por apoiar o crescimento de nossa comunidade. "
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .lightGray
+        label.font = .MCDesignSystem(font: .body)
+        return label
+    }()
+
+    lazy var sendButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Continuar", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .MCDesignSystem(font: .subtitle1)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+        setupView()
+
+        let closeImage = UIImage(named: "Close")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: closeImage,
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(closeAction))
+    }
+
+    private func setupNavBar() {
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sendButton.layer.cornerRadius = 10
+    }
+
+    @objc
+    private func closeAction() {
+        coordinator?.dismiss(self, completion: nil)
+    }
+
+    private func setupView() {
+        view.backgroundColor = .black
+
+        view.addSubview(icon)
+        view.addSubview(subtitle)
+        view.addSubview(bodyText)
+        view.addSubview(sendButton)
+
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        subtitle.translatesAutoresizingMaskIntoConstraints = false
+        bodyText.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        icon.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin).inset(50)
+            make.centerX.equalToSuperview()
+        }
+
+        subtitle.snp.makeConstraints { make in
+            make.top.equalTo(icon.snp.bottom).offset(32)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+        }
+
+        bodyText.snp.makeConstraints { make in
+            make.top.equalTo(subtitle.snp.bottom).offset(6)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+        }
+
+        sendButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottomMargin).inset(32)
+        }
+    }
+
+}
--- Musicall/MusicApp/Coordinator/MainCoordinator.swift
+++ Musicall/MusicApp/Coordinator/MainCoordinator.swift
@@ -23,6 +23,8 @@ class MainCoordinator: Coordinator {
             goToCreatePost()
         case .toReport:
             goToReport()
+        case .toConfirmReport:
+            goToConfirmReport()
         }
     }
 
@@ -34,6 +36,10 @@ class MainCoordinator: Coordinator {
             viewController.coordinator = self
             navigationController?.setViewControllers([viewController], animated: false)
         }
+    }
+
+    func dismiss(_ viewController: UIViewController, completion: (() -> Void)?) {
+        viewController.dismiss(animated: true, completion: completion)
     }
 
     private func goToMural() {
@@ -60,5 +66,16 @@ class MainCoordinator: Coordinator {
         let viewController = ReportViewController()
         viewController.coordinator = self
         navigationController?.pushViewController(viewController, animated: true)
+    }
+
+    private func goToConfirmReport() {
+        let viewController = ConfirmReportViewController()
+        viewController.coordinator = self
+
+        let nvc = UINavigationController()
+        nvc.setViewControllers([viewController], animated: false)
+        nvc.modalPresentationStyle = .fullScreen
+
+        navigationController?.present(nvc, animated: true, completion: nil)
     }
 }
